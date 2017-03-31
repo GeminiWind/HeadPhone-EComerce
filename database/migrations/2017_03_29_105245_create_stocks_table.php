6f696d5633cffc8eda0c7a8af3d78d997d67ad78
nguyentrungthang <nguyenthang084@gmail.com>
2017-03-31 15:45:30 +0700

    trungthang up Models and Migrations

--- database/migrations/2017_03_29_105245_create_stocks_table.php
+++ database/migrations/2017_03_29_105245_create_stocks_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateStocksTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,12 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('stocks', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('import_price')->unsigned();
+            $table->integer('quantity')->default(0);
+            $table->integer('product_id')->unsigned()->unique();
+            $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -30,6 +30,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('stocks');
     }
 }
