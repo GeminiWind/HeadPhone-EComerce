20204ac5e8f072e2691d70ce7735682debbe8e6f
Dung Tham <dungthamkim@gmail.com>
2017-03-31 14:57:30 +0700

    Giao Dien

---
+++ resources/views/layouts/customer/includes/footer.blade.php
@@ -0,0 +1,87 @@
+
+<div id="footer" class="sub-footer" >
+	<div class="container">
+
+		<div class="row">
+			<div class="col-sm-3">
+				<div class="widget">
+					<h4 class="widget-title">Thông tin hỗ trợ</h4>
+					<div>
+						<ul>
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Về chúng tôi</a></li>
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Chính sách đổi trả hàng</a></li>
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Tài khoản ngân hàng</a></li>
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Thông tin tuyển dụng</a></li>
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Thẻ khách hàng thân thiết</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-2">
+				<div class="widget">
+					<h4 class="widget-title">Dịch vụ</h4>
+					<div>
+						<ul>
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i>Quy định bảo hành</a></li>
+							<li><a href="blog_fullwidth_2col.html"><i class="fa fa-chevron-right"></i> Sửa chữa tai nghe</a></li>
+
+						</ul>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-sm-4">
+				<div class="col-sm-10">
+					<div class="widget">
+						<h4 class="widget-title">Thông tin liên hệ</h4>
+						<div>
+							<div class="contact-info">
+								<i class="fa fa-map-marker"></i>
+								<p>Hotline bán hàng: 1900 123 001</p>
+								<p>3D Duy Tân, Hà Nội Phone: +84 123456789</p>
+								<p>Email: sales@tainghehtd.com.vn</p>
+								<p>Khiếu nại dịch vụ: 0900123456</p>
+								<p>Email: info@tainghehtd.com.vn</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-3">
+				<div class="widget">
+					<h4 class="widget-title">Hệ thống cửa hàng</h4>
+					
+				</p>
+				<div>
+							<div class="contact-info">
+								<i class="fa fa-map-marker"></i>
+								<p>3D Duy Tân, Hà Nội Phone: +84 123456789</p>
+								<p>4D Con Rùa, Hà Nội Phone: +84 987654321 </p>
+								
+							</div>
+						</div>
+
+			</div>
+		</div>
+	</div> <!-- .row -->
+</div> <!-- .container -->
+</div> <!-- #footer -->
+<div class="copyright">
+	<center>
+		<i>Giấy phép ĐKKD số: 0105273484 do sở kế hoạch và đầu tư TP Hà Nội cấp ngày 25/04/2020</i>
+	</center>
+</div>
+<div class="copyright">
+
+	<div class="container">
+		<p class="pull-left">Privacy policy. (&copy;) 2017</p>
+		<p class="pull-right pay-options">
+			<a href="#"><img src="customer/images/pay/master.jpg" alt="" /></a>
+			<a href="#"><img src="customer/images/pay/pay.jpg" alt="" /></a>
+			<a href="#"><img src="customer/images/pay/visa.jpg" alt="" /></a>
+			<a href="#"><img src="customer/images/pay/paypal.jpg" alt="" /></a>
+		</p>
+		<div class="clearfix"></div>
+	</div> <!-- .container -->
+
+</div> <!-- .copyright -->
---
+++ resources/views/layouts/customer/includes/head.blade.php
@@ -0,0 +1,14 @@
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Website bán tai nghe</title>
+	<link rel="stylesheet" href="customer/bootstrap3.3.7/css/bootstrap.min.css">
+	<link rel="stylesheet" href="customer/css/font-awesome.min.css">
+	<link rel="stylesheet" href="customer/vendors/colorbox/example3/colorbox.css">
+	<link rel="stylesheet" href="customer/rs-plugin/css/settings.css">
+	<link rel="stylesheet" href="customer/rs-plugin/css/responsive.css">
+	<link rel="stylesheet" title="style" href="customer/css/style.css">
+	<link rel="stylesheet" href="customer/css/animate.css">
+	<link rel="stylesheet" title="style" href="customer/css/mystyle.css">
+
+</head>
