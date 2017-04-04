72ed86b81206bfe9e6f7d1f556b9ec4aa1615dfc
Dung Tham <dungthamkim@gmail.com>
2017-04-04 15:13:28 +0700

    done

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -7,4 +7,25 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     //
+    protected $table = 'products';
+    public function category()
+    {
+        # code...
+        return $this->belongsTo('App\Models\Category');
+
+    }
+
+    public function orderProduct()
+    {
+    	# code...
+    	return $this->belongsToMany('App\Models\OrderProduct','product_id');
+    }
+
+    public function stoke()
+    {
+        # code...
+        return $this->belongsTo('App\Models\Stoke','stoke_id');
+
+    }
+
 }
