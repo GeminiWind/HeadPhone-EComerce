f21adc4178ff878ab1aba93129efe5063bd2fed9
nguyentrungthang <nguyenthang084@gmail.com>
2017-04-04 17:54:05 +0700

    fix conflict

--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,8 @@
 | contains the "web" middleware group. Now create something great!
 |
  */
-<<<<<<< HEAD
 Route::name('index')->get('/', function () {
     return view('customers.home');
-=======
-
-Route::get('/', function () {
-	return view('welcome');
->>>>>>> 1aeb7541e9594eba298d31ee65503b3da34baf4e
 });
 Route::group(['prefix' => 'my'], function () {
     Route::name('change-password')->get('change-password', function () {
@@ -25,12 +19,6 @@ Route::group(['prefix' => 'my'], function () {
     Route::name('change-info')->get('change-info', function () {
         return view('customers.change_infor');
     });
-
-<<<<<<< HEAD
-=======
-Route::get('trang-chu', function () {
-    return view('customers.home');
->>>>>>> 1aeb7541e9594eba298d31ee65503b3da34baf4e
 });
 Route::get('search', function () {
     return view('customers.search');
@@ -39,23 +27,9 @@ Route::group(['namespace' => 'Customer'], function(){
     Auth::routes();
 });
 Route::get('/home', 'HomeController@index');
-
-Route::get('infor', function () {
-    return view('customers.change_infor');
-});
-
-Route::get('search', function () {
-    return view('customers.search');
-});
-
-<<<<<<< HEAD
- //rout vao admin
 Route::group(['prefix' => 'cate'], function(){
     Route::get('add', ['as' =>'admin.category.store', 'uses'=> 'CateController@getAddCate']);
     Route::post('add', ['as' =>'admin.cate-post', 'uses' =>'CateController@postAddCate']);
-=======
-Route::get('test', function () {
-    return view('profile');
 });
 Route::get('dang-nhap',function(){
 	return view('customers.login_customer');
@@ -65,5 +39,4 @@ Route::get('dang-ky',function(){
 });
 Route::get('product-detail', function(){
 	return view('customers.product_detail');
->>>>>>> 1aeb7541e9594eba298d31ee65503b3da34baf4e
 });
